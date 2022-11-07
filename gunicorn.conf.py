b6e55cca2d88820df868092085c78c7694048a44
IyobedZekarias <50155965+IyobedZekarias@users.noreply.github.com>
2022-11-07 13:28:01 -0500

    procfile

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,12 @@
+from http import server
 import multiprocessing
 bind = "127.0.0.1:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 reload = True
 threads = 3
+max_requests = 1000
+timeout = 3600
+# certfile = '/Users/nbstr/ssl/certificate.pem'
+# keyfile = '/Users/nbstr/ssl/key.pem'
+
+# gunicorn -c gunicorn.conf.py wsgi:app
